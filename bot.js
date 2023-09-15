5203422160c05cff0bf1994aacee817cb9cec49e
Sid The Hacker <131943405+sid238@users.noreply.github.com>
2023-09-15 13:59:07 +0530

    update bot.js

--- bot.js
+++ bot.js
@@ -1740,28 +1740,79 @@ break
 
 
 case 'githubstalk': {
-  if (!args[0]) return m.reply(`🕒 Mention a GitHub username to stalk.`);
-  
+  if (!args[0]) return m.reply('Mention a GitHub username to stalk.');
+
   const username = args[0];
-  
-  // Fetch GitHub user data (Replace with your actual GitHub API request)
-  const githubData = await fetch(`https://api.github.com/users/${username}`);
-  const userData = await githubData.json();
 
-  if (userData.message === 'Not Found') {
-    return m.reply(`🚩 GitHub user not found.`);
+  try {
+    // Fetch GitHub user data using Axios
+    const githubResponse = await axios.get(`https://api.github.com/users/${username}`);
+    const userData = githubResponse.data;
+
+    if (githubResponse.status !== 200) {
+      return m.reply(`❌ GitHub user not found.`);
+    }
+
+    // Construct the response message
+    let responseMessage = `🌟 *GitHub Profile - @${userData.login}*\n\n`;
+    responseMessage += `  ◦  *Name*: ${userData.name || 'N/A'}\n`;
+    responseMessage += `  ◦  *Username*: @${userData.login}\n`;
+    responseMessage += `  ◦  *Bio*: ${userData.bio || 'N/A'}\n`;
+    responseMessage += `  ◦  *ID*: ${userData.id}\n`;
+    responseMessage += `  ◦  *Node ID*: ${userData.node_id}\n`;
+    responseMessage += `  ◦  *Profile URL*: ${userData.avatar_url}\n`;
+    responseMessage += `  ◦  *GitHub URL*: ${userData.html_url}\n`;
+    responseMessage += `  ◦  *Type*: ${userData.type}\n`;
+    responseMessage += `  ◦  *Admin*: ${userData.site_admin ? 'Yes' : 'No'}\n`;
+    responseMessage += `  ◦  *Company*: ${userData.company || 'N/A'}\n`;
+    responseMessage += `  ◦  *Blog*: ${userData.blog || 'N/A'}\n`;
+    responseMessage += `  ◦  *Location*: ${userData.location || 'N/A'}\n`;
+    responseMessage += `  ◦  *Email*: ${userData.email || 'N/A'}\n`;
+    responseMessage += `  ◦  *Public Repositories*: ${userData.public_repos}\n`;
+    responseMessage += `  ◦  *Public Gists*: ${userData.public_gists}\n`;
+    responseMessage += `  ◦  *Followers*: ${userData.followers}\n`;
+    responseMessage += `  ◦  *Following*: ${userData.following}\n`;
+    responseMessage += `  ◦  *Created At*: ${userData.created_at}\n`;
+    responseMessage += `  ◦  *Updated At*: ${userData.updated_at}\n`;
+
+   
+ const githubReposData = await fetch(`https://api.github.com/users/${username}/repos?per_page=5&sort=stargazers_count&direction=desc`);
+    const reposData = await githubReposData.json();
+
+    if (reposData.length > 0) {
+      const topRepos = reposData.slice(0, 5); // Display the top 5 starred repositories
+
+      const reposList = topRepos.map(repo => {
+        return `  ◦  *Repository*: [${repo.name}](${repo.html_url})
+  ◦  *Description*: ${repo.description || 'N/A'}
+  ◦  *Stars*: ${repo.stargazers_count}
+  ◦  *Forks*: ${repo.forks}`;
+      });
+
+      const reposCaption = `📚 *Top Starred Repositories*\n\n${reposList.join('\n\n')}`;
+      responseMessage += `\n\n${reposCaption}`;
+    } else {
+      responseMessage += `\n\nNo public repositories found.`;
+    }
+
+    // Send the message with the updated caption and user's avatar
+    await client.sendMessage(m.chat, { image: { url: userData.avatar_url }, caption: responseMessage }, { quoted: m });
+
+    // Add a success reaction message
+    const successReactionMessage = {
+      react: {
+        text: "✔",
+        key: m.key
+      }
+    };
+    await client.sendMessage(m.chat, successReactionMessage);
+  } catch (error) {
+    console.error('Error fetching GitHub data:', error);
+    await client.sendMessage(m.chat, 'An error occurred while fetching GitHub data.', { quoted: m });
   }
-
-  let caption = `乂  *G I T H U B - S T A L K*\n\n`;
-  caption += `  ◦  *Name* : ${userData.name || 'N/A'}\n`;
-  caption += `  ◦  *Username* : ${userData.login}\n`;
-  caption += `  ◦  *Followers* : ${userData.followers}\n`;
-  caption += `  ◦  *Following* : ${userData.following}\n`;
-  caption += `  ◦  *Public Repositories* : ${userData.public_repos}\n\n`;
-  
-  await client.sendMessage(m.chat, { image: { url: userData.avatar_url }, caption: caption }, { quoted: m });
   break;
 }
+
 
 case 'tagall':
     // Fetch group metadata
