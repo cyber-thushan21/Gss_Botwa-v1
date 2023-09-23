ca3f142377dcc3760c88e379119aff427bbd88c3
TSTHUSHAN <123916721+TSTHUSHAN@users.noreply.github.com>
2023-09-23 12:02:36 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -479,23 +479,19 @@ case 'audio':
 
   let captvidAudio = `
 
-╔┉───────────────┉✰
-🧚${tlang().title} 
-
+╔┉───────────────┉✰ 
 🚨 *Youtube Player* 🌿
  ◨┉━━━━╚◭☬◮╝━━━━━┉◧
 
-╏🎀 *Title:* ${anu.title}
+╏🎀 *Title:* ${titleVideo}
 
-╏🌐 *Duration:* ${anu.timestamp}
+╏🌐 *Duration:* ${timestampVideo}
 
-╏👀 *Viewers:* ${anu.views}
+╏👀 *Viewers:* ${viewsVideo}
 
-╏⬆️ *Uploaded:* ${anu.ago}
+╏⬆️ *Uploaded:* ${agoVideo}
 
-╏👽 *Author:* ${anu.author.name}
-
-╏📡 *Url* : ${anu.url}
+╏📡 *Url* : ${urlVideo}
 *𝚂𝙷𝙴𝙽𝚄 𝚀𝚄𝙴𝙴𝙽 𝚂𝙾𝙽𝙶 𝙳𝙾𝚆𝙱𝙻𝙾𝙳 ✅*
 ╚┉────────────────┉✰`;
 
